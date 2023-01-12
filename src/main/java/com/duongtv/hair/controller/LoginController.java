89358dea61d3297a21b3f3b5d235f878f28ccdb8
datran <orion.diget97@gmail.com>
2023-01-12 15:40:44 +0700

    create

---
+++ src/main/java/com/duongtv/hair/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.duongtv.hair.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String index() {
+        return "login";
+    }
+    @PostMapping("/login")
+    public String index(String username,String password) {
+        return "login";
+    }
+}
---
+++ src/test/java/com/duongtv/hair/HairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duongtv.hair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
