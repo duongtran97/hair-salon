48f4382e3bc9d28c9ae01bd9d138856dd5d787d0
datran <orion.diget97@gmail.com>
2023-01-16 23:32:24 +0700

    update application.properties

--- src/main/java/com/duongtv/hair/controller/HomeController.java
+++ src/main/java/com/duongtv/hair/controller/HomeController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.duongtv.hair.repository.UserRepositiry;
+
 @Controller
 public class HomeController {
+    private UserRepositiry userRepositiry;
     @GetMapping("/")
     public String index() {
         return "index";
@@ -16,6 +19,8 @@ public class HomeController {
     }
     @GetMapping("/user")
     public String user() {
+        userRepositiry.findAll();
+
         return "user";
     }
 }
