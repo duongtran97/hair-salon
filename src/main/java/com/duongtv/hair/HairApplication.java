c9cef676235ff9455db9a5e4b38796de5c01ff10
datran <orion.diget97@gmail.com>
2023-01-19 00:27:34 +0700

    add class controller exxception

--- src/main/java/com/duongtv/hair/HairApplication.java
+++ src/main/java/com/duongtv/hair/HairApplication.java
@@ -1,13 +1,30 @@
 package com.duongtv.hair;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.duongtv.hair.entities.UserEntities;
+import com.duongtv.hair.repository.UserRepositiry;
 
 @SpringBootApplication
 public class HairApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(HairApplication.class) ;
 	public static void main(String[] args) {
 		SpringApplication.run(HairApplication.class, args);
 	}
-
+public CommandLineRunner demo(UserRepositiry userRepositiry) {
+	return (args) -> {
+		// fetch all customers
+		logger.info("Customers found with findAll():");
+		logger.info("-------------------------------");
+		for (UserEntities userEntities : userRepositiry.findAll()) {
+			logger.info(userEntities.toString());
+		}
+		logger.info("");
+	};
+}
 }
---
+++ src/main/java/com/duongtv/hair/controller/ExceptionHandlingController.java
@@ -0,0 +1,12 @@
+package com.duongtv.hair.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ExceptionHandlingController {
+    
+    public String logErrorMessage() {
+
+     return "error";   
+    }
+}
