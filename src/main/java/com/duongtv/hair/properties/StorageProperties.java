cc452408d88e56a5eed99465a9350feb3ce63040
duongtran <orion.diget97@gmail.com>
2023-03-20 22:49:04 +0700

    update form add product for admin

---
+++ src/main/java/com/duongtv/hair/properties/StorageProperties.java
@@ -0,0 +1,19 @@
+package com.duongtv.hair.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class StorageProperties {
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
